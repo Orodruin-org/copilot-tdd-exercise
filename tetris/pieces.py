1738bfab78b2771cd5d856e6583deeaa0d827a82
Rasmus Paltschik <91126255+EficodeRjpalt@users.noreply.github.com>
2025-01-22 15:30:14 +0200

    Implement the `rotate` method to rotate the piece 90 degrees
    * Add shape definitions for Tetromino types O, T, S, Z, J, and L
    * Implement rotation logic for Tetromino types T, S, Z, J, and L
    * Handle O piece rotation as a no-op since it does not change shape

--- tetris/pieces.py
+++ tetris/pieces.py
@@ -16,6 +16,18 @@ class Piece:
         # e.g., for an I piece, a simple placeholder could be:
         if self.type == TetrominoType.I:
             self.shape = [(0,0), (0,1), (0,2), (0,3)]
+        elif self.type == TetrominoType.O:
+            self.shape = [(0,0), (0,1), (1,0), (1,1)]
+        elif self.type == TetrominoType.T:
+            self.shape = [(0,1), (1,0), (1,1), (1,2)]
+        elif self.type == TetrominoType.S:
+            self.shape = [(0,1), (0,2), (1,0), (1,1)]
+        elif self.type == TetrominoType.Z:
+            self.shape = [(0,0), (0,1), (1,1), (1,2)]
+        elif self.type == TetrominoType.J:
+            self.shape = [(0,0), (1,0), (1,1), (1,2)]
+        elif self.type == TetrominoType.L:
+            self.shape = [(0,2), (1,0), (1,1), (1,2)]
         else:
             self.shape = []
     
@@ -23,4 +35,15 @@ class Piece:
         """Rotate the piece 90 degrees in place."""
         if self.type == TetrominoType.I:
             self.shape = [(y, -x) for x, y in self.shape]
-        # Implement rotation logic for other Tetromino types as needed
+        elif self.type == TetrominoType.O:
+            pass  # O piece does not change shape on rotation
+        elif self.type == TetrominoType.T:
+            self.shape = [(y, -x) for x, y in self.shape]
+        elif self.type == TetrominoType.S:
+            self.shape = [(y, -x) for x, y in self.shape]
+        elif self.type == TetrominoType.Z:
+            self.shape = [(y, -x) for x, y in self.shape]
+        elif self.type == TetrominoType.J:
+            self.shape = [(y, -x) for x, y in self.shape]
+        elif self.type == TetrominoType.L:
+            self.shape = [(y, -x) for x, y in self.shape]
